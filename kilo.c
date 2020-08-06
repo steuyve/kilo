053311dd396d28cf10f54de70241cb53835d5787
Stephen Liu <stephen.liu@emali.io>
2020-08-06 22:30:18 +0800

    Disable canonical mode and enter raw mode.

--- kilo.c
+++ kilo.c
@@ -1,4 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h> 	/* Character handling functions */
+#include <unistd.h> 	/* Standard symbolic constants and types */
+#include <termios.h> 	/* Terminal interface */
+
+struct termios orig_termios;
+
+void restore_termios_config()
+{
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void raw_mode()
+{
+	// save termios config
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(restore_termios_config);
+
+	struct termios raw = orig_termios;
+	tcgetattr(STDIN_FILENO, &raw);
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main()
 {
+	raw_mode();
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		if (iscntrl(c)) {
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n", c, c);
+		}
+	}
 	return 0;
 }
