eeb111ef76c3446d061b988260d5d35b72c93e15
Stephen Liu <steuyve@gmail.com>
2020-08-09 22:33:55 +0800

    Don't allow scrolling past the end of a line (+1 space for appending).

--- kilo.c
+++ kilo.c
@@ -383,12 +383,15 @@ void process_keypress(void) {
 
 void move_cursor(int key)
 {
+	/* get the current row. */
+	erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+	
 	switch (key) {
 		case ARROW_LEFT:
 			if (E.cx != 0) E.cx--;
 			break;
 		case ARROW_RIGHT:
-			E.cx++;
+			if (row && E.cx < row->size) E.cx++;
 			break;
 		case ARROW_UP:
 			if (E.cy != 0) E.cy--;
