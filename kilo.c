fdebb0ab304f309a9d6f9c06ec67e7b5b94f67eb
Stephen Liu <steuyve@gmail.com>
2020-08-10 10:24:06 +0800

    Make page up and page down scroll up and down a screen and end key go to end of line.

--- kilo.c
+++ kilo.c
@@ -416,11 +416,19 @@ void process_keypress(void) {
 			E.cx = 0;
 			break;
 		case END_KEY:
-			E.cx = E.screencols - 1;
+			if (E.cy < E.numrows)
+				E.cx = E.row[E.cy].size;
 			break;
 		case PAGE_UP:
 		case PAGE_DOWN:
 			{
+				if (c == PAGE_UP) {
+					E.cy = E.rowoff;
+				} else if (c == PAGE_DOWN) {
+					E.cy = E.rowoff + E.screenrows - 1;
+					if (E.cy > E.numrows) E.cy = E.numrows;
+				}
+
 				int times = E.screenrows;
 				while (times--) move_cursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
 			}
